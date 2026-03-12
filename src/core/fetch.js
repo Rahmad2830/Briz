e05df227326e660a07f07835b5190ee814268e93
Rahmad2830 <rahmatnurhidayat0101@gmail.com>
2026-03-12 16:48:58 +0700

    handle when response is redirect

--- src/core/fetch.js
+++ src/core/fetch.js
@@ -56,6 +56,10 @@ export async function $fetch(url, params = {}) {
     }, manualReqTimeout)
     const res = await fetch(request.url, request.options)
     if(!res.ok) throw new Error(await res.text())
+    if(res.redirected) {
+      window.location.href = res.url
+      return
+    }
     
     dispatchZEvent("z:request-success", { request, response: res }, meta.el)
     
